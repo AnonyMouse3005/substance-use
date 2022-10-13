14858e862562b6226189d25db5f694b5069a620c
AnonyMouse3005 <trannguyen300599@gmail.com>
2022-10-13 13:08:28 -0500

    learning curves for each combination of feature groups

---
+++ data/original/C2-readme.txt
@@ -0,0 +1,7 @@
+There are two changes to be aware of from C1 to C2.
+
+First, the name of the variable that captured the number of people a person has used drugs with in the past 30 days has changed.
+In cohort 2 this variable is listed as NSD1 whereas it was listed as ND1 or was a duplicate name in cohort 1. 
+
+Second, we added 5 new substances for non-injection drug use in C2W2 that are not present in other waves.
+I included them for you, as I wasn’t sure if they would be useful or not. They are C2W2_17 – C2W2_21.
--- helper.py
+++ helper.py
@@ -1,7 +1,33 @@
 from itertools import chain, combinations
+import numpy as np
+from sklearn.model_selection import LeaveOneOut
+from sklearn.preprocessing import StandardScaler
+import json
 
 
 def powerset(iterable):
     # Usage: list(powerset([1,2,3])) --> [(), (1,), (2,), (3,), (1,2), (1,3), (2,3), (1,2,3)]
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def loocv(X, y, model):
+    loo = LeaveOneOut()
+    scores = []
+    for train_index, test_index in loo.split(X):
+        # print("TRAIN:", train_index, "TEST:", test_index)
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        clf = model.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+        # print(score)
+        scores.append(score)
+
+    return np.mean(scores)
+
+def standard_scale(X):
+    scaler = StandardScaler().fit(X)
+    return scaler.transform(X)
+
+def json_dump(filepath, var):
+    with open(filepath, 'w') as f:
+        json.dump(var, f)
